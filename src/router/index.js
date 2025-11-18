08aa0c7ecdbc2dedccf1029183605c3e8a9cfae8
SerjoMhana <serjomh33@gmail.com>
2025-11-18 19:06:33 +0200

    second  commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import AboutView from '../views/AboutView.vue'
 // import AboutUsView from '../views/AboutUsView.vue'  // Removed about-us page
@@ -9,7 +9,7 @@ import HistoryView from '../views/HistoryView.vue'
 import SupportView from '../views/SupportView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
